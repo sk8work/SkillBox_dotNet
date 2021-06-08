4ccafbb18882d3c036742eca0d37beac35af1674
sk8work <eshikhantsev@gmail.com>
2021-06-08 05:21:25 +0300

    Completed HomeWork5 HW4

--- Module1/005/HomeWork5/HW4/CheckProgression.cs
+++ Module1/005/HomeWork5/HW4/CheckProgression.cs
@@ -9,7 +9,7 @@ namespace HW4
 {
     class CheckProgression
     {
-        int count = 100;
+        int count = 10;
         public CheckProgression()
         {
             for (int i = 0; i < count; i++)
@@ -18,8 +18,18 @@ namespace HW4
                 Sleep();
                 Console.WriteLine($"Исходный сгенерированный массив №{i+1}: ");
                 PrintArr(createArray.Arr);
+                Console.WriteLine($"Является ли массив числовой последовательнстью и арифметической прогрессией: {IsArrAriphmeticProgression(createArray.Arr)}");
+                Console.WriteLine($"Является ли массив числовой последовательнстью и геометрической прогрессией: {IsArrGometricProgression(createArray.Arr)}");
                 PrintString();
             }
+
+            // Примеры, показывающие заранее известный результат (контрольные примеры)
+            //Console.WriteLine($"Является ли массив числовой последовательнстью и арифметической прогрессией: {IsArrAriphmeticProgression(new int[] { 1,2,3,4,5,6})}"); //true
+            //Console.WriteLine($"Является ли массив числовой последовательнстью и геометрической прогрессией: {IsArrGometricProgression(new int[] { 1, 2, 3, 4, 5, 6 })}"); //false
+
+            //Console.WriteLine($"Является ли массив числовой последовательнстью и арифметической прогрессией: {IsArrAriphmeticProgression(new int[] { 1, 2, 4, 8, 16, 32 })}"); //false
+            //Console.WriteLine($"Является ли массив числовой последовательнстью и геометрической прогрессией: {IsArrGometricProgression(new int[] { 1, 2, 4, 8, 16, 32 })}"); // true
+
         }
 
         /// <summary>
@@ -27,7 +37,7 @@ namespace HW4
         /// </summary>
         private void Sleep()
         {
-            Thread.Sleep(100);
+            Thread.Sleep(300);
         }
 
         /// <summary>
@@ -50,8 +60,16 @@ namespace HW4
         /// <returns></returns>
         public bool IsArrAriphmeticProgression(int[] arr)
         {
-            bool isTrue = false;
-
+            bool isTrue = true;
+            int length = arr.Length;
+            for (int i = 1; i < length-1; i++)
+            {
+                if (arr[i] != (arr[i-1] + arr[i+1])/2)
+                {
+                    isTrue = false;
+                    break;
+                }
+            }
 
             return isTrue;
         }
@@ -63,8 +81,18 @@ namespace HW4
         /// <returns></returns>
         public bool IsArrGometricProgression(int[] arr)
         {
-            bool isTrue = false;
+            bool isTrue = true;
+            int length = arr.Length;
+            for (int i = 1; i < length - 1; i++)
+            {
+                if (arr[i] * arr[i] != (arr[i - 1] * arr[i + 1]))
+                {
+                    isTrue = false;
+                    break;
+                }
+            }
 
+            return isTrue;
 
             return isTrue;
         }
