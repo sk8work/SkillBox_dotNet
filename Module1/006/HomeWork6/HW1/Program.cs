94bed6963faf8d5771d1d3a1586f78817f63331d
sk8work <eshikhantsev@gmail.com>
2021-10-30 17:48:50 +0300

    Completed HW 6

--- Module1/006/HomeWork6/HW1/Program.cs
+++ Module1/006/HomeWork6/HW1/Program.cs
@@ -22,10 +22,12 @@ using System;
 using System.IO;
 using System.Text;
 
+
 namespace HW1
 {
     class Program
     {
+        
         /// <summary>
         /// Delay
         /// </summary>
@@ -34,17 +36,127 @@ namespace HW1
             Console.ReadKey();
         }
 
-        static void Main(string[] args)
+        static int id = 0;
+
+        /// <summary>
+        /// Записываем данные в файл
+        /// </summary>
+        /// <param name="path">FileName (или путь к файлу)</param>
+        static void Writer(string path)
         {
-
-
-            string empListFile = "empList.txt";
-
-            using (FileStream fs = new(empListFile, FileMode.OpenOrCreate))
+            if(File.Exists(path))
             {
+                id = 0;
+                using (StreamReader sr = new StreamReader(path, Encoding.Unicode))
+                {
+                    string line;
 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        id += 1;
+                    }
+                }
+            }
+            
+
+            using (StreamWriter ss = new StreamWriter(path, true, Encoding.Unicode))
+            {
+                
+
+                string note = string.Empty;
+
+                note += $"{Convert.ToString(id)}\t";
+
+                note += $"{DateTime.Now.ToShortDateString()}/{ DateTime.Now.ToShortTimeString()}\t";
+
+                Console.WriteLine("\nВведите Ф.И.О.: ");
+                note += $"{Console.ReadLine()}\t";
+
+                Console.Write("Введите Возраст: ");
+                note += $"{Console.ReadLine()}\t";
+
+                Console.Write("Введите Рост: ");
+                note += $"{Console.ReadLine()}\t";
+
+                Console.Write("Введите Дату рождения: ");
+                note += $"{Console.ReadLine()}\t";
+
+                Console.Write("Введите Место рождения: ");
+                note += $"{Console.ReadLine()}\t";
+
+                ss.WriteLine($"{note}");
+            }
+        }
+
+        /// <summary>
+        /// Читаем из файла
+        /// </summary>
+        /// <param name="path">FileName (или путь к файлу)</param>
+        static void Reader(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path, Encoding.Unicode))
+                {
+                    string line;
+                    Console.WriteLine($"\n{"ID",-5}{"Дата и время",-20}{"Ф. И. О.",-20}{"Возраст",-10}{"Рост",-7}{"Дата рождения",-17}{"Место рождения",-17}");
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] data = line.Split('\t');
+                        Console.WriteLine($"\n{data[0],-5}{data[1],-20}{data[2],-20}{data[3],-10}{data[4],-7}{data[5],-17}{data[6],-17}");
+                    }
+                }
+            } else
+            {
+                Console.WriteLine("\nФайл пуст или ге существует. Заполните файл!");
             }
 
+            
+        }
+
+        /// <summary>
+        /// Вывод меню программы
+        /// </summary>
+        /// <param name="path">FileName (или путь к файлу)</param>
+        static void Game(string path)
+        {
+            var game = true;
+            while (game)
+            {
+                Console.WriteLine("Программа учета сотрудников компании");
+                Console.WriteLine("Меню программы: ");
+                Console.WriteLine("1 - Добавить сотрудника.");
+                Console.WriteLine("2 - Вывести список сотрудников.");
+                Console.WriteLine("q - Выйти из программы");
+                Console.Write(">> ");
+                char choice = ' ';
+                choice = Console.ReadKey().KeyChar;
+                switch (choice)
+                {
+                    case '1':
+                        {
+                            Writer(path);
+                            break;
+                        }
+                    case '2':
+                        {
+                            Reader(path);
+                            break;
+                        }
+                    case 'q':
+                        {
+                            game = false;
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine("\nGood bye!!!");
+        }
+
+        static void Main(string[] args)
+        {
+            Game("db.csv");
 
             // Delay
             Delay();
