5a02e1f8782a5256431b3e75dce81cc94da3f150
sk8work <eshikhantsev@gmail.com>
2021-11-18 17:23:16 +0300

    Continued 07-hw project

--- HW/HW-07/HW-07-new/FileWorker.cs
+++ HW/HW-07/HW-07-new/FileWorker.cs
@@ -9,36 +9,26 @@ namespace HW_07_new
 {
     struct FileWorker
     {
-        /// <summary>
-        /// Метод для чтения из файла
-        /// </summary>
-        /// <param name="path">Путь к файлу</param>
-        static void FileReader(string path)
-        {
-            using (StreamReader sr = new StreamReader(path))
-            {
-                string line = String.Empty;
-                Console.WriteLine($"{"ID",4} {"Дата создания",18} {"Время создания",18} {"ФИО",20} {"Возраст",8} {"Возраст",8} {"Рост",5} {"Дата рождения",15} {"Место рождения",15}");
 
-                while ((line = sr.ReadLine()) != null)
+        static void Writer(string path)
+        {
+            if (File.Exists(path))
+            {
+                UserInfo.Id = 0;
+                using (StreamReader sr = new StreamReader(path, Encoding.Unicode))
                 {
-                    string[] data = line.Split('#');
-                    Console.WriteLine($"{data[0],4} {data[1],18} {data[2],18} {data[3],20} {data[4],8} {data[5],8} {data[6],5} {data[7],15} {data[8],15}");
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        UserInfo.Id += 1;
+                    }
                 }
             }
-        }
 
-        /// <summary>
-        /// Метод для записи сотрудника в файл
-        /// </summary>
-        /// <param name="path">Путь к файлу</param>
-        /// <param name="note">Строка для записи в файл</param>
-        static void FileWriter(string path, string note)
-        {
-            using (StreamWriter sw = new StreamWriter(path, true))
-            {
-                sw.WriteLine(note);
-            }
+            UserInfo.GetUserInfo();
+            string noteToWrite = UserInfo.GetUserString(UserInfo.Id, UserInfo.Fio, UserInfo.Age, UserInfo.High, UserInfo.BirthDate, UserInfo.BirthPlace);
+            WriteStreamToFile(path, noteToWrite);
         }
     }
 }
---
+++ HW/HW-07/HW-07-new/UserInfo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_07_new
+{
+    struct UserInfo
+    {
+        static int id = 0;
+        static string fio = String.Empty;
+        static int age = 0;
+        static int high = 0;
+        static DateTime birthDate = new DateTime(1990, 01, 01);
+        static string birthPlace = String.Empty;
+
+        public static int Id { get => id; set => id = value; }
+        public static string Fio { get => fio; set => fio = value; }
+        public static int Age { get => age; set => age = value; }
+        public static int High { get => high; set => high = value; }
+        public static DateTime BirthDate { get => birthDate; set => birthDate = value; }
+        public static string BirthPlace { get => birthPlace; set => birthPlace = value; }
+
+        /// <summary>
+        /// Формируем строку с данными от пользователя
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fio"></param>
+        /// <param name="age"></param>
+        /// <param name="high"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="birthPlace"></param>
+        /// <returns></returns>
+        public static void GetUserInfo()
+        {
+            Console.Write("\nВведите Ф.И.О.: ");
+            Fio = Console.ReadLine();
+
+            Console.Write("Введите Возраст: ");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите Рост: ");
+            High = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите Дату рождения: ");
+            BirthDate = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Write("Введите Место рождения: ");
+            BirthPlace = Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Формируем строку с данными от пользователя
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fio"></param>
+        /// <param name="age"></param>
+        /// <param name="high"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="birthPlace"></param>
+        /// <returns></returns>
+        public static string GetUserString(int id, string fio, int age, int high, DateTime birthDate, string birthPlace)
+        {
+            string note = string.Empty;
+
+            note += $"{id}#{DateTime.Now.ToShortDateString()}/{ DateTime.Now.ToShortTimeString()}#{fio}#{age}#{high}#{birthDate}#{birthPlace}";
+            return note;
+        }
+    }
+}
